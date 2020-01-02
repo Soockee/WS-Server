6715aedadd25869c51a9edf590ec9d1298ec513d
Simon Stockhause <gamingsockee@gmail.com>
2020-01-02 20:06:29 +0100

    added diffrent images

--- WS-Server/Server.cs
+++ WS-Server/Server.cs
@@ -12,27 +12,50 @@ namespace WS_Server
 {
     class Server
     {
-        private const int frameRate = 300;
+        private const int frameRate = 31;
+        private const String imagesBaseURL = @"../../../images";
+        private enum Images
+        { 
+            Earth,
+            Yoda,
+            BabyYoda,
+        }
         public static void Main()
         {
             FleckLog.Level = LogLevel.Debug;
             var allSockets = new List<IWebSocketConnection>();
             var server = new WebSocketServer("ws://0.0.0.0:8181");
-            var allImages = new List<ImageData>();
-            Directory.GetFiles(@"../../../images").ToList().ForEach(file => allImages.Add(new ImageData(Path.GetFileName(file), file)));
+            Images selection = Images.Earth;
+
+            List<ImageData> allImages = getSelectedImageList(selection,imagesBaseURL);
+
+            Task sendImage = null;
+            Boolean taskCompleted = false;
+            Boolean selectNewImages = false;
+
+            // Define the selectNewImages token.
+            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
 
             Action sendDelayedIamges = () => {
                 var enm = allImages.GetEnumerator();
                 while (enm.MoveNext())
                 {
+                    if (allSockets.Count == 0) {
+                        source.Cancel();
+                        Console.WriteLine("Cancelling at task: no sockets left");
+                        break;
+                    }
+                    else if(selectNewImages == true)
+                    {
+                        source.Cancel();
+                        Console.WriteLine("Cancelling at task: selectNewImages set by user");
+                        break;
+                    }                  
                     allSockets.ForEach(s => s.Send(enm.Current.getImage()));
                     Thread.Sleep(frameRate);
                 }
             };
-
-            Task sendImage = null;
-            Boolean taskCompleted = false;
-
 
             server.Start(socket =>
             {
@@ -40,9 +63,11 @@ namespace WS_Server
                 {
                     Console.WriteLine("Open!");
                     allSockets.Add(socket);
-                    sendImage = Task.Factory.StartNew(sendDelayedIamges, CancellationToken.None, TaskCreationOptions.DenyChildAttach,
-                        TaskScheduler.Default);
-                    taskCompleted = sendImage.IsCompleted;
+                    if (sendImage == null) { 
+                        sendImage = Task.Factory.StartNew(sendDelayedIamges, token, TaskCreationOptions.DenyChildAttach,
+                            TaskScheduler.Default);
+                        taskCompleted = sendImage.IsCompleted;
+                    }
                 };
                 socket.OnClose = () =>
                 {
@@ -54,11 +79,33 @@ namespace WS_Server
                     if (message.Equals("exit")) 
                     {
                         socket.Close();
+                    }   
+                    else if (message.Equals("earth"))
+                    {
+                        selection = Images.Earth;
+                        selectNewImages = true;
                     }
-                    Console.WriteLine(message);
+                    else if (message.Equals("yoda"))
+                    {
+                        selection = Images.Yoda;
+                        selectNewImages = true;
+                    }
+                    else if (message.Equals("babyyoda"))
+                    {
+                        selection = Images.BabyYoda;
+                        selectNewImages = true;
+                    }
+                    if (selectNewImages) 
+                    {
+                        allImages = getSelectedImageList(selection, imagesBaseURL);
+                        sendImage.Wait();
+                        selectNewImages = false;
+                        taskCompleted = sendImage.IsCompleted;
+                        sendImage = Task.Factory.StartNew(sendDelayedIamges, CancellationToken.None, TaskCreationOptions.DenyChildAttach,
+                        TaskScheduler.Default);
 
-                    //while (message != "exit") {
-                    // }
+                    }
+                    Console.WriteLine(message);                 
                 };
             });
 
@@ -74,6 +121,25 @@ namespace WS_Server
                 //input = Console.ReadLine();
             }
         }
-      
+        private static List<ImageData> getSelectedImageList(Images selection, String imagesBaseURL) 
+        {
+            List<ImageData> list = new List<ImageData>();
+
+            switch (selection) 
+            {
+                case Images.Earth:
+                    imagesBaseURL+="/earth";
+                    break;
+                case Images.Yoda:
+                    imagesBaseURL += "/yoda";
+                    break;
+                case Images.BabyYoda:
+                    imagesBaseURL += "/babyyoda";
+                    break;
+
+            }
+            Directory.GetFiles(imagesBaseURL).ToList().ForEach(file => list.Add(new ImageData(Path.GetFileName(file), file)));
+            return list;
+        }
     }
 }
